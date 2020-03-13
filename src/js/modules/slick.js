a109724740b86746bd5321baef5fb5063c9837fd
odinokun <odinokun@gmail.com>
2020-03-13 08:14:22 +0200

    did team

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -22,6 +22,26 @@ module.exports = function() {
     ]
   });
 
+  $('#team-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
   // end Slick slider
 
 };
